461a056f7e4f7f22838c2c3e935f328dc45431d9
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-06-14 17:07:22 -0300

    customizing response for users and admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :authenticate_admin!
+    skip_before_action :verify_authenticity_token
   
+    before_action :set_default_response_format
+
+    private
+    def set_default_response_format
+        request.format = :json unless devise_controller? && admin_controller_actions?
+      end
+    
+    def admin_controller_actions?
+        (controller_name == 'sessions' || controller_name == 'registrations') && (action_name == 'new' || action_name == 'create')
+    end
 end
